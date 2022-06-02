c20d5a6f394664ca6a02afb7bee1a22d287b2529
Siva kumar <59410636+Sivakumar001@users.noreply.github.com>
2022-06-02 12:13:27 +0530

    added stack queue set

--- cppstl.cpp
+++ cppstl.cpp
@@ -1,60 +1,67 @@
-#include<bits/stdc++.h>
-#define LOG(x) :: std::cout<< x <<std::endl;
+#include <bits/stdc++.h>
+#define LOG(x) ::std::cout << x << std::endl;
 
 using namespace std;
 
-int main(){
-    /* Array 
+int main()
+{
+    /* Array
     it is similar to normal declaration but convenient in access/copy
     can be passed in function by value
     can use for iterator based calculation below
     */
     array<int, 5> supra_int;
-    
-    for(int i=0;i<5;i++){
+
+    for (int i = 0; i < 5; i++)
+    {
         supra_int[i] = i;
     }
     LOG(supra_int.size());
-    LOG(supra_int.at(3)); // 3
-    LOG(supra_int.empty()); //false
-    LOG(supra_int.front()); //0
-    LOG(supra_int.back()); //4
+    LOG(supra_int.at(3));   // 3
+    LOG(supra_int.empty()); // false
+    LOG(supra_int.front()); // 0
+    LOG(supra_int.back());  // 4
 
-    for(int* i=supra_int.begin(); i!=supra_int.end(); i++){
+    for (int *i = supra_int.begin(); i != supra_int.end(); i++)
+    {
         LOG(*i);
     }
     LOG("***vector****")
 
-    /* Vector 
+    /* Vector
     dynamic array
     uses dynamic sizes to power of 2
     */
     vector<int> vct;
-    
+
     LOG(vct.capacity());
-   
-    for(int i=0;i<10;i++){
-       vct.push_back(i);
+
+    for (int i = 0; i < 10; i++)
+    {
+        vct.push_back(i);
     }
-    
+
     LOG(vct.size());
     LOG(vct.capacity());
-    
+
     vct.pop_back();
     LOG(vct.back());
 
-    vct.insert(vct.begin()+3, 88);
-    for(int i=0;i<10;i++){
-       LOG(vct[i]);
+    vct.insert(vct.begin() + 3, 88);
+    for (int i = 0; i < 10; i++)
+    {
+        LOG(vct[i]);
     }
 
-    for(std::vector<int>::iterator i = vct.begin(); i!=vct.end(); i++){
+    for (std::vector<int>::iterator i = vct.begin(); i != vct.end(); i++)
+    {
         LOG(*i);
     }
     vct.clear();
     LOG(vct.size());
-    vector<int> a(5,1);
-    for(int i: a){
+    vector<int> a(5, 1);
+    for (int i : a)
+    {
         LOG(i);
     }
     LOG("****deque****")
@@ -70,14 +77,15 @@ int main(){
     dq.push_back(464);
     dq.push_front(167);
     LOG(dq.size());
-    
+
     dq.pop_back();
     dq.pop_front();
     LOG(dq.size());
 
-    dq.erase(dq.begin(), dq.end()-1);
+    dq.erase(dq.begin(), dq.end() - 1);
 
-    for(deque<int>::iterator i= dq.begin(); i!=dq.end(); i++){
+    for (deque<int>::iterator i = dq.begin(); i != dq.end(); i++)
+    {
         LOG(*i);
     }
 
@@ -90,24 +98,118 @@ int main(){
     number_list.pop_front();
     number_list.pop_back();
 
-    for(std::list<int>::iterator i=number_list.begin(); i!=number_list.end(); i++){
+    for (std::list<int>::iterator i = number_list.begin(); i != number_list.end(); i++)
+    {
         LOG(*i);
     }
-    
+
     std::list<int>::iterator itr = number_list.begin();
     advance(itr, 1);
 
-    number_list.erase(itr , number_list.end());
+    number_list.erase(itr, number_list.end());
     LOG("after removal")
-    for(std::list<int>::iterator i=number_list.begin(); i!=number_list.end(); i++){
+    for (std::list<int>::iterator i = number_list.begin(); i != number_list.end(); i++)
+    {
         LOG(*i);
     }
     LOG(number_list.size());
 
-    /* Stack 
-    First In Last Out
+    /* Stack
+    Last In First Out
     bookstack,
     ctrl+z,
     browser back button,
     */
+
+    stack<string> stck;
+    stck.push("Siva");
+    stck.push("kumar");
+
+    LOG(stck.size());
+
+    LOG(stck.top());
+    stck.pop();
+    LOG(stck.top());
+
+    LOG(stck.empty());
+
+    LOG("****Queue*****");
+    /* Queue
+    First In First Out
+    spotify playlist,
+    ticket queue,
+    */
+
+    queue<string> q;
+    q.push("al;kwhjrgnawrg");
+    q.push("siva");
+    q.push("kumar");
+
+    LOG(q.size());
+
+    q.pop();
+    LOG(q.back());
+
+    LOG(q.empty());
+    LOG("*****Priority Queue*****")
+    /* Priority Queue === Max Heap/ Min Heap
+    stores value based on ascending order whatever the user enters
+    cares who is on top just like capitalism
+    */
+
+    priority_queue<int> maxh; //default is max heap
+    priority_queue<int, vector<int>, greater<int>> minh;
+
+    maxh.push(1);
+    maxh.push(488);
+    maxh.push(4);
+    maxh.push(50);
+
+    int size = maxh.size();
+    for(int i=0;i<size; i++){
+        LOG(maxh.top());
+        maxh.pop();
+    }
+
+    minh.push(0);
+    minh.push(100);
+    minh.push(1000);
+    minh.push(43);
+
+    for(int i=0;i<size;i++){
+        LOG(minh.top());
+        minh.pop();
+    }
+
+    LOG(maxh.empty());
+    LOG(minh.empty());
+
+    LOG("*****set*****");
+    /* Set 
+    elements are unique in nature
+    cant modify
+    ordered set/ unordered set
+    */
+
+    set<int> st;
+    st.insert(2);
+    st.insert(1);
+    st.insert(3);
+    st.insert(5);
+    st.insert(7);
+
+    for(set<int>::iterator i=st.cbegin(); i!=st.cend(); i++){
+        LOG(*i);
+    }
+    LOG(st.size());
+    
+    unordered_set<int> unset;
+    unset.insert(3);
+    unset.insert(1);
+    unset.insert(5);
+    unset.insert(4);
+    unset.insert(2);
+    for(auto itr=unset.begin(); itr!=unset.end();itr++){
+        LOG(*itr);
+    }
 }
