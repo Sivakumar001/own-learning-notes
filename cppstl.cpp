781629534f9bf4d0d4111ae3bdfc16e10871b664
Siva kumar <59410636+Sivakumar001@users.noreply.github.com>
2022-06-06 12:38:48 +0530

    added sort, swap, rotate, reverse functions

--- cppstl.cpp
+++ cppstl.cpp
@@ -250,6 +250,22 @@ int main()
     LOG(binary_search(alvct.begin(), alvct.end(), 11)); //binary search
 
     LOG(*(lower_bound(alvct.begin(), alvct.end(), 3)));
+    LOG(*(upper_bound(alvct.begin(), alvct.end(), 4)));
+    
+    int x = 10;
+    int y = 20;
+    swap(x, y);
+    
+    LOG(max(x, y));
+    LOG(min(x, y));
 
-    LOG(typeid(begin(alvct)).name());
+    string s = "siva";
+    reverse(s.begin(), s.end());
+    LOG(s);
+
+    vector<int> b = {1,2,3,4,5,6,7};
+    rotate(b.begin(), b.begin()+2, b.end());
+    for(auto i: b){
+        LOG(i);
+    }
 }
