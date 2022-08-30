072c6f6ee9fe5393f5af2ca7defd72c2573f4704
Siva kumar <59410636+Sivakumar001@users.noreply.github.com>
2022-08-30 19:28:47 +0530

    implementation of linked list

--- takeuforward-dsa/basics of dsa/patterns/pat9.cpp
+++ takeuforward-dsa/basics of dsa/patterns/pat9.cpp
@@ -1,14 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 int main(){
     int n;
     cin >> n;
+    int width=2*n;
 
     for(int i=1;i<=n;i++){
-        for(int j=0;j<i;j++){
-            cout << char('A'+j);
+        for(int j=1;j<=n;j++){
+            if(j<=i){
+                cout << j;
+            }
+            else{
+                cout << " ";
+            }
+        }
+        for(int j=n;j>0;j--){
+            if(j>i){
+                cout << " ";
+            }else{
+                cout << j;
+            }
         }
         cout << "\n";
     }
