056df2be36cb15fc2c1b4e54a7470321c59c673f
Siva kumar <59410636+Sivakumar001@users.noreply.github.com>
2022-10-14 23:03:04 +0530

    some changes

--- takeuforward-dsa/day 10/nqueens.cpp
+++ takeuforward-dsa/day 10/nqueens.cpp
@@ -1,59 +1,53 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-bool isQueenSafe(vector<string> &board, int row, int col){
-    int duprow=row;
-    int dupcol=col;
 
-    while(duprow>=0 && dupcol>=0){
-        if(board[duprow][dupcol]=='Q')return false;
-        duprow--;
-        dupcol--;
-    }
 
-    duprow=row;
-    dupcol=col;
-
-    while(dupcol>=0){
-        if(board[duprow][dupcol]=='Q')return false;
-        dupcol--;
-    }
-
-    duprow=row;
-    dupcol=col;
-
-    while(duprow<board.size() && dupcol>=0){
-        if(board[duprow][dupcol]=='Q')return false;
-        duprow++;
-        dupcol--;
-    }
-
-    return true;
-}
-
-void Nqueen(vector<vector<string>> &answer, vector<string> &board, int total_queen, int col=0){
+void Nqueen(vector<vector<string>> &answer,
+            vector<string> &board, 
+            vector<bool> &colcheck,
+            vector<bool> &upperdiagonal,
+            vector<bool> &lowerdiagonal,
+            int col=0){
+    
     if(col==board.size()){
         answer.push_back(board);
         return;
     }
+
     for(int row=0; row<board.size();row++){
-        if(isQueenSafe(board, row, col)){
-            board[row][col] = 'Q';
-            Nqueen(answer, board, total_queen, col+1);
-            board[row][col] = '.';
+        if(colcheck[row]==0 &&
+           upperdiagonal[row+col]==0 &&
+           lowerdiagonal[(board.size() -1)+col-row]==0){
+            
+            colcheck[row]=1;
+            upperdiagonal[row+col]=1;
+            lowerdiagonal[(board.size()-1)+col-row]=1;
+        
+            board[row][col]='Q';
+            
+            Nqueen(answer, board, colcheck, upperdiagonal, lowerdiagonal, col+1);
+            
+            board[row][col]='.';
+            
+            colcheck[row]=0;
+            upperdiagonal[row+col]=0;
+            lowerdiagonal[(board.size()-1)+col-row]=0;
         }
     }
 }
 
 int main(void){
-    int number=5;
-    vector<string> board(number);
+    int number=4;
     string s(number, '.');
-    for(int i=0;i<number;i++){
-        board[i]=s;
-    }
+    vector<string> board(number, s);
     vector<vector<string>> answer;
-    Nqueen(answer, board, number);
+    vector<bool> colcheck(number, 0);
+    vector<bool> upperdiagonal((2*number)-1, 0);
+    vector<bool> lowerdiagonal((2*number)-1, 0);
+
+    Nqueen(answer, board, colcheck, upperdiagonal, lowerdiagonal);
+    
     for(auto it: answer){
         for(auto itr: it){
             cout << itr << endl;
